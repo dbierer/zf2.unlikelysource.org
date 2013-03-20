4709a654f3aa1d0a1a2f08a13bc6b660df901779
dbierer <garavatar@unlikelysource.com>
2013-03-20 14:34:01 +0000

    Removed ClassFile and replaced with Forum; updated layout

--- public/version.php
+++
@@ -1,6 +0,0 @@
-<?php
-require '../vendor/ZF2/library/Zend/Version/Version.php';
-$version = new Zend\Version\Version();
-echo 'ZF Version: ' . $version::VERSION;
-echo '<br />';
-echo md5('hoohah'); 
